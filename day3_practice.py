cd7ecc31d4bdb27fcacf294ab98182c16f749c35
Pushpa Tadi <pushpatadi8@gmail.com>
2026-07-03 13:41:00 +0530

    Add practice exercises in day3_practice.py

---
+++ day3_practice.py
@@ -0,0 +1,37 @@
+ #Practice-1
+name = "sreekar"
+age = 18
+city = "Bhimavaram"
+ 
+
+print(name)
+print(age)
+print(city)
+
+# Practice-2
+name = "sreeker"
+age = 19
+city = "hyderabad"
+ 
+print(name)
+print(age)
+print(city)
+
+#Practice-3
+country = "India"
+print(country)
+
+
+# Practice-4
+name = "Sreeker"
+age = 19
+city = "Hyderabad"
+country = "India"
+favorite_color = "Black"
+
+print(name)
+print(age)
+print(city)
+print(country)
+print(favorite_color)
+
