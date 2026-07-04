7f54d129a46980f45fa07b35d8e3625fc90c7571
Pushpa Tadi <pushpatadi8@gmail.com>
2026-07-04 10:28:01 +0530

    Added Student Profile Card Mini project

---
+++ student_profile_card.py
@@ -0,0 +1,17 @@
+# Student Profile Card
+
+name = "Pushpa"
+age = 18
+height = 5.4
+is_student = True
+fav_color = "Black"
+fav_food = "Biryani"
+
+print("===== STUDENT PROFILE CARD =====")
+print("Name        :", name)
+print("Age         :", age)
+print("Height      :", height)
+print("Student     :", is_student)
+print("Favorite Color :", fav_color)
+print("Favorite Food  :", fav_food)
+print("===============================")
