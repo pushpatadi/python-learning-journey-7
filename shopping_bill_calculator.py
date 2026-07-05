175dab593745196d26b188d5e19fb7ce3c7a70cf
Pushpa Tadi <pushpatadi8@gmail.com>
2026-07-05 10:33:06 +0530

    Completed Day 5 - Type Conversion and Shopping Bill Project

---
+++ shopping_bill_calculator.py
@@ -0,0 +1,16 @@
+item = "Notebook"
+price = "230"
+quantity = "7"
+
+price = int(price)
+quantity = int(quantity)
+
+total = price * quantity
+
+
+print("====SHOPPING BILL====")
+print("Item:", item)
+print("Price:", price)
+print("Quantity:", quantity)
+print("Total Bill:", total)
+
