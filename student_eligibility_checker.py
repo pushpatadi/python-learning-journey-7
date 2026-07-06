2da978593aad27577ec961abf98e8c7ddb4f7dc1
Pushpa Tadi <pushpatadi8@gmail.com>
2026-07-06 12:23:39 +0530

    Completed Day 6 - Student_Eligibility_Checker

---
+++ student_eligibility_checker.py
@@ -0,0 +1,18 @@
+# Student Eligibility Checker
+
+student_name = "Pushpa
+age = 18
+attendence = 90
+
+print("==== STUDENT ELIGIBILITY CHECKER ====")
+print("Student Name", student_name)
+print("Age:", age)
+print("Attendance:", attendance)
+
+print("Age >=18 :", age >= 18)
+print("Attendance >= 75 :", attendance >= 75)
+
+eligible = (age >= 18) and (attendance >= 75)
+
+print("Eligibile:", eligible)
+
