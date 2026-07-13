874dd577a737134ba87dbdec2034eb0f8c78715b
Pushpa Tadi <pushpatadi8@gmail.com>
2026-07-13 12:43:14 +0530

    Added String Analyzer mini project

---
+++ String_Analyzer.py
@@ -0,0 +1,9 @@
+word = input("Enter a word: ")
+
+print("Original Word : ",word[:])
+
+print("First 3 Letters : ",word[0:2])
+
+print("Last 3 Letters : ",word[-3:])
+
+print("Every 2nd Letter : ",word[0: :2])
